46914cd8c4dbf707a6aa881c486dffc82ab06771
Keenan Masic <keenan.masic@student.kyh.se>
2020-09-10 18:17:15 +0200

    changed value

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz
 {
     public static void main(String[] args)
     {
-        for(int i = 0; i < 101; i++)
+        for(int i = 1; i < 101; i++)
 
         {
             if(i % 3 == 0 && i % 5 == 0) {System.out.println("FizzBuzz");}
