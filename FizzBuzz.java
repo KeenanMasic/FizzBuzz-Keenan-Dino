09f2a166f24c4ffececb8578473b2534e583ff11
Keenan Masic <keenan.masic@student.kyh.se>
2020-09-11 22:18:25 +0200

    changed formatting and variable

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -7,15 +7,20 @@ public class FizzBuzz
         for(int i = 1; i < 101; i++)
 
         {
-            if(i % 3 == 0 && i % 5 == 0) {System.out.println("FizzBuzz");}
+            if(i % 3 == 0 && i % 5 == 0)
+                {System.out.println("FizzBuzz");}
 
-            if(i == 42) {System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");}
+            else if(i == 42)
+                {System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");}
 
-            else if(i % 3 == 0) {System.out.println("Fizz");}
+            else if(i % 3 == 0)
+                {System.out.println("Fizz");}
 
-            else if(i % 5 == 0) {System.out.println("Buzz");}
+            else if(i % 5 == 0)
+                {System.out.println("Buzz");}
 
-            else {System.out.println(i);}
+            else
+                {System.out.println(i);}
         }
 
     }
